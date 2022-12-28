c56ed75aeab0cc7711ab72ea4e38c81b16b1c2ac
therishav <rishavchatterjee9@gmail.com>
2022-12-28 23:28:02 +0530

    solution created

---
+++ src/webparts/d3Demo/components/ID3DemoProps.ts
@@ -0,0 +1,7 @@
+export interface ID3DemoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/d3Demo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ID3DemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'D3DemoWebPartStrings' {
+  const strings: ID3DemoWebPartStrings;
+  export = strings;
+}
