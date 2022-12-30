d0139ce8f4838e3e49fbd57caeca4e02033333f6
therishav <rishavchatterjee9@gmail.com>
2022-12-30 09:25:52 +0530

    Added scale and axis to line chart

--- src/webparts/d3Demo/components/Line.tsx
+++ src/webparts/d3Demo/components/Line.tsx
@@ -1,6 +1,17 @@
 import * as React from "react";
 import { useEffect, useRef, useState } from "react";
-import { select, line, curveCardinal } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  scaleLinear,
+  NumberValue,
+  Axis,
+  axisRight,
+} from "d3";
+
+require("./Styles.css");
 
 function Line() {
   const svgRef = useRef();
@@ -8,16 +19,34 @@ function Line() {
 
   useEffect(() => {
     const svg = select(svgRef.current);
+
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+
+    const xAxis: any = axisBottom(xScale).ticks(data.length);
+
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+
+    const yAxis: any = axisRight(yScale);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    // generates the "d" attribute of a path element
     const myLine = line<any>()
-      .x((value, index, data) => index * 50)
-      .y((value) => value)
+      .x((value, index, data) => xScale(index))
+      .y((value) => yScale(value))
       .curve(curveCardinal);
 
+    // renders path element, and attaches
+    // the "d" attribute from line generator above
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
       .attr("d", (value) => myLine(value))
+      .attr("class", "line")
       .attr("fill", "none")
       .attr("stroke", "blue");
   }, [data]);
@@ -26,24 +55,28 @@ function Line() {
     <>
       <svg ref={svgRef}>
         {/* <path d="M0, 150 100, 100 150, 120" stroke="blue" fill="none" /> */}
-        <br />
-        <button
-          onClick={() => {
-            let arr = data.map((value) => value + 5);
-            console.log(arr);
-            setData(arr);
-          }}
-        >
-          {"Update data"}
-        </button>
-        <button
-          onClick={() => {
-            setData(data.filter((value) => value < 35));
-          }}
-        >
-          {"Filter data"}
-        </button>
+        <g className="x-axis" />
+        <g className="y-axis" />
       </svg>
+      <br />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          let arr = data.map((value) => value + 5);
+          console.log(arr);
+          setData(arr);
+        }}
+      >
+        {"Update data"}
+      </button>
+      <button
+        onClick={() => {
+          setData(data.filter((value) => value < 35));
+        }}
+      >
+        {"Filter data"}
+      </button>
     </>
   );
 }
