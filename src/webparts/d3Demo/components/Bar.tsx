322d8df4dd4155c5ced39ef0dbced8a985111799
therishav <rishavchatterjee9@gmail.com>
2023-01-25 23:57:33 +0530

    added chart bars

--- src/webparts/d3Demo/components/Bar.tsx
+++ src/webparts/d3Demo/components/Bar.tsx
@@ -11,6 +11,9 @@ import {
   axisRight,
   scaleBand,
 } from "d3";
+import styles from "./D3Demo.module.scss";
+import { values } from "lodash";
+
 require("./Styles.css");
 
 function Bar() {
@@ -20,9 +23,12 @@ function Bar() {
   useEffect(() => {
     const svg = select(svgRef.current);
 
-    const xScale = scaleBand().domain([0, 1, 2, 3, 4, 5, 6]).range([0, 300]);
+    const xScale = scaleBand()
+      .domain([0, 1, 2, 3, 4, 5, 6])
+      .range([0, 300])
+      .padding(0.5);
 
-    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+    const yScale: any = scaleLinear().domain([0, 150]).range([150, 0]);
 
     const xAxis: any = axisBottom(xScale).ticks(data.length);
 
@@ -31,13 +37,16 @@ function Bar() {
     const yAxis: any = axisRight(yScale);
     svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
 
-    svg.selectAll(".bar")
-    .data(data)
-    .join("rect")
-    .att("class", "bar")
-    .att("x", (value: any, index: any) => xScale(index));
-
-}, [data]);
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .attr("x", (value: any, index: any) => xScale(index))
+      .attr("y", yScale)
+      .attr("width", xScale.bandwidth())
+      .attr("height", (value) => 150 - yScale(value));
+  }, [data]);
 
   return (
     <>
@@ -46,25 +55,24 @@ function Bar() {
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
-      <br />
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          let arr = data.map((value) => value + 5);
-          console.log(arr);
-          setData(arr);
-        }}
-      >
-        {"Update data"}
-      </button>
-      <button
-        onClick={() => {
-          setData(data.filter((value) => value < 35));
-        }}
-      >
-        {"Filter data"}
-      </button>
+      <div className={styles.chartBtn}>
+        <button
+          onClick={() => {
+            let arr = data.map((value) => value + 5);
+            console.log(arr);
+            setData(arr);
+          }}
+        >
+          {"Update data"}
+        </button>
+        <button
+          onClick={() => {
+            setData(data.filter((value) => value < 35));
+          }}
+        >
+          {"Filter data"}
+        </button>
+      </div>
     </>
   );
 }
